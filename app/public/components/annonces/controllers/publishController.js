a7d19ba179b9644057b168f613aab9e86ee5e20d
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2016-02-22 14:49:42 +0100

    Finish submit advert form

--- app/public/components/annonces/controllers/publishController.js
+++ app/public/components/annonces/controllers/publishController.js
@@ -1,34 +1,127 @@
 'use strict';
 
 angular.module('TimeShareSilex')
-  .controller('publishCtrl', ['$http', function($http, uib) {
+	.controller('publishCtrl', ['$http', '$scope', function($http, $scope) {
 
-	var vm = this;
-	vm.sentOK = false;
-	vm.sentError = false;
-	vm.dt = new Date();
-  
-	vm.submitAdvert = function(valid) {
-		if (valid) {
-			vm.advert['user'] = { 'id': '569d06ecc4936293a6f8fd90'};
-			vm.advert['demande'] = false;
+		var vm = this;
 
-			$http.post('/api/annonces/', vm.advert).then(
-				function (response) {
-					if (response.status === 201) {
-						vm.sentOK = true;
-					}
-				},
-				function () {
-					vm.sentError = true;
-				});
-		}
+		vm.dateValiditeDebut = new Date(); // current date
+		vm.dateValiditeFin = new Date();
+		vm.dateValiditeFin.addMonths(1); // validity of one month by default
+		vm.minDate = new Date(); // disable use date in the past
 
-	};
+		vm.alert = {
+			type: '',
+			msg: ''
+		}; // default altert message for uib-alert
 
-	vm.dateOptions = {
-		startingDay: 1,
-		formatYear: 'yy'
-	}
+		vm.isDisabled = false; // to later disable submit button
 
-}]);
+		// uib-datepicker-popup options
+		vm.dateOptions = {
+			startingDay: 1,
+			formatYear: 'yy'
+		};
+
+		vm.validateDates = function() {
+			var endDate = new Date(vm.dateValiditeFin);
+			var startDate = new Date(vm.dateValiditeDebut);
+			$scope.advertForm.startDate.$setValidity('endBeforeStart', endDate >= startDate);
+		};
+
+		vm.submitAdvert = function(valid) {
+			if (valid) {
+				// fake user TODO: change to use a real user
+				vm.advert.user = {
+					'id': '569d06ecc4936293a6f8fd90'
+				};
+
+				vm.advert.demande = false;
+
+				// construct ISO date string from date object
+				var d = vm.dateValiditeDebut;
+				var day = ('0' + d.getDate()).slice(-2); // force 2 digits
+				var month = ('0' + (d.getMonth() + 1)).slice(-2); // force 2 digits
+				var year = d.getFullYear();
+				vm.advert.dateValiditeDebut = year + '-' + month + '-' + day;
+
+				// construct ISO date string from date object
+				var f = vm.dateValiditeFin;
+				day = ('0' + f.getDate()).slice(-2); // force 2 digits
+				month = ('0' + (f.getMonth() + 1)).slice(-2); // force 2 digits
+				year = f.getFullYear();
+				vm.advert.dateValiditeFin = year + '-' + month + '-' + day;
+
+				$http({
+					method: 'POST',
+					url: '/api/annonces/',
+					data: vm.advert
+				}).then(
+					function(response) {
+						if (response.status === 201) {
+							vm.alert = {
+								type: 'success',
+								msg: 'Annonce publiée'
+							};
+							vm.isDisabled = true; // disable submit button
+						} else if (response.status === 400) {
+							vm.alert = {
+								type: 'danger',
+								msg: 'Erreur: annonce non publiée'
+							};
+						} else if (response.status === 404) {
+							vm.alert = {
+								type: 'danger',
+								msg: 'Erreur: annonce non publiée'
+							};
+						}
+					},
+					function() {
+						vm.alert = {
+							type: 'danger',
+							msg: 'Erreur serveur'
+						};
+					});
+			} else {
+				vm.alert = {
+					type: 'danger',
+					msg: 'Un champ requis est manquant'
+				};
+			}
+
+		};
+
+		vm.closeAlert = function() {
+			vm.alert = {
+				type: '',
+				msg: ''
+			};
+		};
+
+	}]);
+
+
+// taken from datejs library to be able to add a month
+Date.isLeapYear = function(year) {
+	return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+};
+
+Date.getDaysInMonth = function(year, month) {
+	return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+};
+
+Date.prototype.isLeapYear = function() {
+	return Date.isLeapYear(this.getFullYear());
+};
+
+Date.prototype.getDaysInMonth = function() {
+	return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+};
+
+Date.prototype.addMonths = function(value) {
+	var n = this.getDate();
+	this.setDate(1);
+	this.setMonth(this.getMonth() + value);
+	this.setDate(Math.min(n, this.getDaysInMonth()));
+	return this;
+};
